cb1a9115b2736c751a67266caa3f62ac622b832c
Vital3414 <qwer1234432148@gmail.com>
2023-12-12 18:50:57 +0200

    fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,43 +1,3 @@
-// import { combineReducers, configureStore } from '@reduxjs/toolkit';
-// import { contactsReducer } from './contacts/contactSlice';
-// import {
-//   FLUSH,
-//   REHYDRATE,
-//   PAUSE,
-//   PERSIST,
-//   PURGE,
-//   REGISTER,
-// } from 'redux-persist';
-// import { persistStore, persistReducer } from 'redux-persist';
-// import storage from 'redux-persist/lib/storage';
-// import { filterReducer } from './contacts/filterSlice';
-
-// const rootReducer = combineReducers({
-//   contacts: contactsReducer,
-//   filter: filterReducer,
-// });
-
-// const persistConfig = {
-//   key: 'contacts',
-//   storage,
-//   blacklist: ['filter'],
-// };
-
-// const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-// export const store = configureStore({
-//   reducer: persistedReducer,
-//   middleware: getDefaultMiddleware =>
-//     getDefaultMiddleware({
-//       serializableCheck: {
-//         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-//       },
-//     }),
-// });
-
-// export const persistor = persistStore(store);
-
-// store.js
 import { configureStore } from '@reduxjs/toolkit';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
@@ -66,7 +26,6 @@ const persistedFilterReducer = persistReducer(
 const rootReducer = {
   contacts: persistedContactsReducer,
   filter: persistedFilterReducer,
-  // Додайте інші редуктори за потреби
 };
 
 const store = configureStore({
